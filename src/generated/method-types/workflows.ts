0acb5e1017ac2c1bd1555fee74678d233d116c8e
Neil Shapiro <nshapiro@slack-corp.com>
2022-06-09 12:21:50 -0700

    Missed file

--- src/generated/method-types/workflows.ts
+++ src/generated/method-types/workflows.ts
@@ -3,5 +3,8 @@ import { SlackAPIMethod } from "../../types.ts";
 export type WorkflowsAPIType = {
   stepCompleted: SlackAPIMethod;
   stepFailed: SlackAPIMethod;
+  triggers: {
+    create: SlackAPIMethod;
+  };
   updateStep: SlackAPIMethod;
 };
