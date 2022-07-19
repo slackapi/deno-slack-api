dc50711b28b4bea05bf0d9ce8bb449e4bbc09bff
Neil Shapiro <nshapiro@slack-corp.com>
2022-07-19 12:11:16 -0700

    Add trigger response typing

--- src/typed-method-types/workflows/triggers/mod.ts
+++ src/typed-method-types/workflows/triggers/mod.ts
@@ -1,10 +1,10 @@
 import { BaseMethodArgs, BaseResponse } from "../../../types.ts";
 import { EventTrigger } from "./event.ts";
+// TODO: Rename to `scheduled.ts` after the Schedule PR is merged
 import { ScheduledTrigger } from "./schedule.ts";
 import { ShortcutTrigger } from "./shortcut.ts";
 import { WebhookTrigger } from "./webhook.ts";
-
-// TODO: Export to SDK
+import { FilterType } from "./trigger-filter.ts";
 export const TriggerTypes = {
   Event: "event",
   Scheduled: "scheduled",
@@ -39,7 +39,87 @@ type ValidTriggerTypes =
   | ShortcutTrigger
   | WebhookTrigger;
 
-type BaseTriggerResponse = Promise<BaseResponse>;
+export type BaseTriggerResponse = Promise<
+  SuccessfulTriggerResponse | FailedTriggerResponse
+>;
+
+// TODO: Test this response with all trigger types
+type SuccessfulTriggerResponse =
+  & BaseResponse
+  & Pick<BaseTrigger, "name" | "description">
+  & {
+    ok: true;
+    /** @description The name of the trigger */
+    /** @description A timestamp of when the trigger was created */
+    date_created: number;
+    /** @description A timestamp of when the workflow was last updated */
+    date_updated: number;
+    /** @description successful calls come with the returned trigger object */
+    trigger: Pick<BaseTrigger, "type"> & {
+      /** @description The trigger's internal uid */
+      id: string;
+      /** @description The metadata for the workflow */
+      function: {
+        /** @description the workflow function's internal uid */
+        id: string;
+        /** @description the workflow's internal uid */
+        workflow_id: string;
+        /** @description the workflow's callback_id */
+        callback_id: string;
+        /** @description the workflow's title */
+        title: string;
+        /** @description the workflow's description */
+        description: string;
+        /** @default "workflow" */
+        type: string;
+        // TODO: Add generic for inputs
+        /** @description the workflow's input parameters */
+        input_parameters: {
+          // deno-lint-ignore no-explicit-any
+          [key: string]: any;
+        }[];
+        // TODO: Add generic for outputs
+        /** @description the workflow's output parameters */
+        output_parameters: {
+          // deno-lint-ignore no-explicit-any
+          [key: string]: any;
+        }[];
+        /** @description the app_id that the workflow belongs to */
+        app_id: string;
+        /** @description the app metadata that the workflow belongs to */
+        app: {
+          id: string;
+          name: string;
+          icons: {
+            image_32: string;
+            image_48: string;
+            image_64: string;
+            image_72: string;
+          };
+        };
+        /** @description A timestamp of when the workflow was last updated */
+        date_updated: number;
+      };
+      // TODO: Type this based on a generic
+      /** @description The inputs provided to the workflow */
+      inputs: {
+        [key: string]: {
+          value: unknown;
+          locked: boolean;
+          hidden: boolean;
+        };
+      };
+      // TODO: Is this needed on any response?
+      filter: FilterType;
+    };
+  };
+
+type FailedTriggerResponse = BaseResponse & {
+  ok: false;
+  /** @description no trigger is returned on a failed response */
+  trigger?: never;
+};
+
 type TriggerResponse = BaseTriggerResponse;
 
 export type TypedWorkflowsTriggersMethodTypes = {
