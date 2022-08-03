50e85a7a1004fa6e630a342aa9cd17ff28a10ee5
Daniel Hwang <daniel.hwang@salesforce.com>
2022-08-02 17:09:23 -0700

    split event triggers into Channel/Workspace and Regular/Meta type events

--- src/typed-method-types/workflows/triggers/event.ts
+++ src/typed-method-types/workflows/triggers/event.ts
@@ -17,30 +17,78 @@ export const TriggerEventTypes = {
   ...MetadataEventTypes,
 } as const;
 
-type TriggerEventTypeValues = ObjectValueUnion<typeof TriggerEventTypes>;
+//Channel level events
+export const ChannelEventTypes = {
+  MessageMetadataPosted: "slack#/events/message_metadata_posted", //channel
+  ReactionAdded: "slack#/events/reaction_added", //channel
+  UserJoinedChannel: "slack#/events/user_joined_channel", //channel
+} as const;
+
+//Workspace level events
+export const WorkspaceEventTypes = {
+  MessageMetadataPosted: "slack#/events/message_metadata_posted", //channel
+  ChannelCreated: "slack#/events/channel_created", //workspace
+  DndUpdated: "slack#/events/dnd_updated", //workspace
+  UserJoinedTeam: "slack#/events/user_joined_team", //workspace
+} as const;
+
+type ChannelLevelEventValues = ObjectValueUnion<typeof ChannelEventTypes>;
+type WorkspaceLevelEventValues = ObjectValueUnion<typeof WorkspaceEventTypes>;
 type MetadataEventTypeValues = ObjectValueUnion<typeof MetadataEventTypes>;
 
-type BaseEvent = {
+type BaseChannelEvent =
+  & (MetadataChannelEvent | ChannelEvent)
+  & {
+    /** @description The channel id's that this event listens on */
+    channel_ids?: string[];
+  };
+
+type ChannelEvent = BaseEvent & {
   /** @description The type of event */
-  event_type: Exclude<TriggerEventTypeValues, MetadataEventTypeValues>;
+  event_type: Exclude<ChannelLevelEventValues, MetadataEventTypeValues>;
+};
+type MetadataChannelEvent =
+  & {
+    /** @description The type of event */
+    event_type: Extract<ChannelLevelEventValues, MetadataEventTypeValues>;
+  }
+  & MetadataEvents
+  & Pick<BaseEvent, "filter">;
+
+type BaseWorkspaceEvent =
+  & (MetadataWorkspaceEvent | WorkspaceEvent)
+  & {
+    /** @description The team id's that this event listens on */
+    team_ids?: string[];
+  };
+
+type WorkspaceEvent = BaseEvent & {
+  /** @description The type of event */
+  event_type: Exclude<WorkspaceLevelEventValues, MetadataEventTypeValues>;
+};
+
+type MetadataWorkspaceEvent =
+  & {
+    /** @description The type of event */
+    event_type: Extract<WorkspaceLevelEventValues, MetadataEventTypeValues>;
+  }
+  & MetadataEvents
+  & Pick<BaseEvent, "filter">;
+
+type BaseEvent = {
   filter?: FilterType;
-  channel_ids?: string[];
-  team_ids?: string[];
   metadata_event_type?: never;
 };
 
 type MetadataEvents = {
-  /** @description The type of event */
-  event_type: Extract<TriggerEventTypeValues, MetadataEventTypeValues>;
   /** @description User defined description for the event type */
-  metadata_event_type: string; // TODO: Is this just a normal string? Is it required?
-} & Omit<BaseEvent, "event_type" | "metadata_event_type">;
-
+  metadata_event_type: string;
+};
 export type EventTrigger =
   & BaseTrigger
   & RequiredInputs
   & {
     type: typeof TriggerTypes.Event;
     /** @description The payload object for event triggers */
-    event: BaseEvent | MetadataEvents;
+    event: BaseChannelEvent | BaseWorkspaceEvent;
   };
