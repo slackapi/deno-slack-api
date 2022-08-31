a2122371c1e4192ccca911f3bfe7ed1e85d59878
Neil Shapiro <nshapiro@slack-corp.com>
2022-08-31 13:30:00 -0700

    Add descriptions

--- src/typed-method-types/chat.ts
+++ src/typed-method-types/chat.ts
@@ -1,11 +1,15 @@
 import { BaseResponse } from "../types.ts";
 
 type ChatPostMessageOptionalArgs = {
+  /** @description The formatted text of the message to be published. If blocks are included, this will become the fallback text used in notifications. */
   text?: string;
+  /** @description A JSON-based array of structured attachments. */
   // deno-lint-ignore no-explicit-any
   attachments?: any[];
+  /** @description A JSON-based array of structured blocks. */
   // deno-lint-ignore no-explicit-any
   blocks?: any[];
+  /** @description Provide another message's ts value to make this message a reply. Avoid using a reply's ts value; use its parent instead. */
   thread_ts?: string;
   // deno-lint-ignore no-explicit-any
   [otherOptions: string]: any;
@@ -20,6 +24,7 @@ type ChatPostMessageOneOfRequired =
   >;
 
 type ChatPostMessageArgs = ChatPostMessageOneOfRequired & {
+  /** @description Channel, private group, or IM channel to send message to. Can be an encoded ID, or a name. */
   channel: string;
 };
 
