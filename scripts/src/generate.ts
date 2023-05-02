6875570713c34f9a38bb06201429166f181ac8c0
William Bergamin <wbergamin@slack-corp.com>
2023-05-02 13:39:00 -0400

    fix format issues

--- scripts/src/generate.ts
+++ scripts/src/generate.ts
@@ -1,4 +1,4 @@
-import { pascalCase, emptyDir, ensureDir } from "../../src/deps.ts";
+import { emptyDir, ensureDir, pascalCase } from "../../src/deps.ts";
 import { APIMethodNode } from "./api-method-node.ts";
 import { getPublicAPIMethods } from "./public-api-methods.ts";
 
@@ -91,16 +91,16 @@ const getGroupCode = (groupNode: APIMethodNode) => {
   ${groupNode.getTypesCode()}
 `;
   if (groupCode.match(/SlackAPIMethod[,;]/)) {
-    imports = '{ SlackAPIMethod';
+    imports = "{ SlackAPIMethod";
   }
   if (groupCode.match(/SlackAPICursorPaginatedMethod[,;]/)) {
     if (imports !== null) {
-      imports += ', SlackAPICursorPaginatedMethod';
+      imports += ", SlackAPICursorPaginatedMethod";
     } else {
-      imports = '{ SlackAPICursorPaginatedMethod';
+      imports = "{ SlackAPICursorPaginatedMethod";
     }
   }
-  imports += ' }';
+  imports += " }";
 
   return `import type ${imports} from "../../types.ts";\n${groupCode}`;
 };
