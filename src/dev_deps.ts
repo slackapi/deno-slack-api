67d6502135166f85af0d4f77c0b03b7e8896abf8
William Bergamin <wbergamin@slack-corp.com>
2023-05-02 13:29:13 -0400

    update import map script

--- src/dev_deps.ts
+++ src/dev_deps.ts
@@ -7,3 +7,7 @@ export {
 } from "https://deno.land/std@0.132.0/testing/asserts.ts";
 export * as mf from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
 export { isHttpError } from "https://deno.land/std@0.182.0/http/http_errors.ts";
+export {
+  afterEach,
+  beforeAll,
+} from "https://deno.land/std@0.185.0/testing/bdd.ts";
