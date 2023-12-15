b735fec174024c4cbcb11c25bfd966becbea44b8
kawasaki.taiga <kawasaki.taiga@raccoon.ne.jp>
2023-12-15 21:35:08 +0900

    Add fileUploadV2 method to BaseSlackAPIClient
    and add new tests to api_test.ts.

--- src/dev_deps.ts
+++ src/dev_deps.ts
@@ -4,6 +4,7 @@ export {
   assertExists,
   assertInstanceOf,
   assertRejects,
+  fail,
 } from "https://deno.land/std@0.132.0/testing/asserts.ts";
 export * as mf from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
 export { isHttpError } from "https://deno.land/std@0.182.0/http/http_errors.ts";
